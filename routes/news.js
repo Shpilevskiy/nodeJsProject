e71fad8ac18d2b0015698b3f0e22d0b281bf98e8
Nick <arioxon@gmail.com>
2016-12-17 16:04:38 +0300

    News route updated

--- routes/news.js
+++ routes/news.js
@@ -4,11 +4,33 @@ var router = express.Router();
 var UserModel = require('../models/user').UserModel;
 var config = require('../config');
 
-router.get('/', function (req, res) {
-    // var newsArticle = req.query.loginError
-    // res.json('status: removed')
+var dogsJson = require('../json_data/dogs.json'); // get article file
+var catsJson = require('../json_data/cats.json'); // get article file
+var rabbitsJson = require('../json_data/rabbits.json'); // get article file
 
-    res.render('newsPage')
+
+router.get('/', function (req, res) {
+    var newsArticle = req.query.article;
+
+    var resultAnswer;
+    if (newsArticle == dogsJson.page) resultAnswer = dogsJson;
+    if (newsArticle == catsJson.page) resultAnswer = catsJson;
+    if (newsArticle == rabbitsJson.page) resultAnswer = rabbitsJson;
+
+
+    if (resultAnswer) res.json(resultAnswer);
+    else res.json("Error: error");
+
+    res.render('newsPage', {
+        title: resultAnswer.title,
+        date: resultAnswer.date,
+        rate: resultAnswer.rate,
+        text: resultAnswer.text,
+        category: resultAnswer.category,
+        sources: resultAnswer.sources,
+        author: resultAnswer.author
+    }
+    )
 });
 
 module.exports = router;
