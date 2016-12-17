9fc925d38f628e725867c9ceb0d2ac20962f0a50
Nick <arioxon@gmail.com>
2016-12-17 16:14:40 +0300

    added middleware for news router

--- routes/news.js
+++ routes/news.js
@@ -8,6 +8,30 @@ var dogsJson = require('../json_data/dogs.json'); // get article file
 var catsJson = require('../json_data/cats.json'); // get article file
 var rabbitsJson = require('../json_data/rabbits.json'); // get article file
 
+router.use(function (req, res, next) {
+    var token = req.cookies.auth;
+
+    if (token) {
+        jwt.verify(token, config.secret, function(err, decoded) {
+            if (err) {
+                return res.redirect('/?loginError=true');
+            } else {
+                // if everything is good, save to request for use in other routes
+                console.log(decoded);
+                req.decoded = decoded;
+                next();
+            }
+        });
+    }
+    else {
+
+        // if there is no token
+        // return an error
+        console.log("bad");
+        return res.redirect('/?loginError=true');
+
+    }
+});
 
 router.get('/', function (req, res) {
     var newsArticle = req.query.article;
